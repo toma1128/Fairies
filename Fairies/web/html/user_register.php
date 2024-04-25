fbc7856005844be8bde2d10909bd81a85d87fdc2
haruna <haru_kawagishi@icloud.com>
2024-04-25 10:25:58 +0900

    クラス名を追加

--- Fairies/web/html/user_register.php
+++ Fairies/web/html/user_register.php
@@ -82,59 +82,68 @@ if ($result["register"]) {
 
 <body>
     <header>
-        <h1>新規作成画面</h1>
+        <h1 class="logo">
+            <img src="images/fairieshome.png" alt="ロゴ" width="230">
+        </h1>
+        <ul>
+            <li><a class="form_link" href="">ログアウト</a></li>
+        </ul>
+
     </header>
     <main>
-        <form id="registrationForm" action="fairiesproject_register.php" method="POST" class="w-1/2 mx-8">
-            <div class="container">
-                <label for="team">所属門又はチーム:</label>
-                <select name="team" id="team">
-                    <option value="aaa">A</option>
-                    <option value="bbb">B</option>
-                    <option value="ccc">C</option>
-                    <option value="ddd">D</option>
-                </select>
-                <div>
-                    <label for="uname">お名前</label><br>
-                    <input type="text" name="uname" id="uname" placeholder="名前を入力してください。" required>
-                </div>
-                <div>
-                    <label for="ruby">フリガナ</label><br>
-                    <input type="text" name="ruby" id="ruby" placeholder="フリガナを入力してください。" required>
-                </div>
-                <div>
-                    <label for="password">パスワード</label><br>
-                    <input type="password" name="password" id="password" placeholder="8文字以上の英数字" required>
-                </div>
-                <div>
-                    <label for="confirmPassword">パスワード（確認）</label><br>
-                    <input type="password" name="confirmPassword" id="confirmPassword" placeholder="8文字以上の英数字" required>
-                </div>
-                <div>
-                    <label for="number">社員番号</label><br>
-                    <input type="number" name="number" id="number" placeholder="例:99999" required>
-                </div>
-                <div>
-                    <label for="birthday">所属生年月日</label><br>
-                    <input type="date" id="birthday" name="birthday" required>
-                </div>
+        <div>
+            <div class="select">
+                <h2>新規作成（従業員用）</h2>
             </div>
-            <button id="submit" type="submit">登録</button>
-            <!-- 新規登録画面で失敗した場合、ダイアログにその結果を表示される用-->
-            <dialog id="resultDialog">
-                <div id="dialogWrap">
-                    <form action="" method="post" id="addItemForm">
-                        <div class="form-item-wrap">
-                            <div class="form-item">
-                                <?= $result["message"] ?><br>
-                            </div>
+            <form id="registrationForm" action="fairiesproject_register.php" method="POST" class="w-1/2 mx-8">
+                <div class="container" class="select">
+                    <label for="team">所属門又はチーム:</label>
+                    <p>半角数字で入力してください。</p>
+                    <select name="team" id="team">
+                        <option value="1">営業</option>
+                        <option value="2">設計</option>
+                        <option value="3">施工管理</option>
+                        <option value="4">事務</option>
+                        <option value="5">積算</option>
+                    </select>
+                    <div class="select">
+                        <label for="number">社員番号</label><br>
+                        <input type="number" name="number" id="number" placeholder="例:99999" required>
+                    </div>
+                    <div class="select">
+                        <div>
+                            <label for="uname">お名前</label><br>
+                            <input type="text" name="uname" id="uname" placeholder="名前を入力してください。" required>
                         </div>
-                        <div class="form-item-wrap"><button id="addItem" type="submit">確認</button></div>
-                    </form>
+                        <div>
+                            <label for="password">パスワード</label><br>
+                            <p>記号と英数字をそれぞれ一文字以上を含めて、八文字以上で入力してください.</p>
+                            <input type="password" name="password" id="password" placeholder="8文字以上の英数字" required>
+                        </div>
+                    </div>
+
+                    <div class="select">
+                        <label for="birthday">入社日</label><br>
+                        <input type="date" id="birthday" name="birthday" required>
+                    </div>
                 </div>
-                <span class="material-icons">close</span>
-            </dialog>
-        </form>
+                <button id="submit" type="submit">登録</button>
+                <!-- 新規登録画面で失敗した場合、ダイアログにその結果を表示される用-->
+                <dialog id="resultDialog">
+                    <div id="dialogWrap">
+                        <form action="" method="post" id="addItemForm">
+                            <div class="form-item-wrap">
+                                <div class="form-item">
+                                    <?= $result["message"] ?><br>
+                                </div>
+                            </div>
+                            <div class="form-item-wrap"><button id="addItem" type="submit">確認</button></div>
+                        </form>
+                    </div>
+                    <span class="material-icons">close</span>
+                </dialog>
+            </form>
+        </div>
 </body>
 
 </html>
