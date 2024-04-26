321ece21186b5230ae4d0f4a1b3302cce4c98676
haruna <haru_kawagishi@icloud.com>
2024-04-26 14:05:22 +0900

    画像の追加

--- Fairies/php/login.php
+++ Fairies/php/login.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_USER','fairies');//ユーザ名
-define('DB_PASS','daimonia');//パスワード
+define('DB_USER', 'fairies'); //ユーザ名
+define('DB_PASS', 'daimonia'); //パスワード
 
 // $userId_employee = filter_input(INPUT_POST,"userId_employee");
 // $password_employee = filter_input(INPUT_POST,"password_employee", FILTER_VALIDATE_INT);
@@ -13,60 +13,61 @@ define('DB_PASS','daimonia');//パスワード
  * @param string $str
  * @return bool
  */
-function is_not_space(?string $str): bool {
+function is_not_space(?string $str): bool
+{
   $str = preg_replace("/( |　)/", "", $str);
   if ($str == "") {
-      return FALSE;
+    return FALSE;
   } else {
-      return TRUE;
+    return TRUE;
   }
 }
 
-if(isset($_POST["submitBtn1"])){
-  try{
+if (isset($_POST["submitBtn1"])) {
+  try {
 
-      $db = new PDO($dsn, DB_USER, DB_PASS);
+    $db = new PDO($dsn, DB_USER, DB_PASS);
 
-      $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  
-      $db->setAttribute(PDO::ATTR_AUTOCOMMIT, false);
-  
-      $sql = 'SELECT * FROM COMPANY EMPLOYEES';
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-      $where = "where COMPANY EMPLOYEES NUMBER = :COMPANY EMPLOYEES NUMBER";
-      $AND = "AND PASSWORD = :PASSWORD";
+    $db->setAttribute(PDO::ATTR_AUTOCOMMIT, false);
 
-      $stmt = $db->prepare($sql.$where.$AND);
-      if($userId_employee && $password_employee){
-        //値のバインド
-        $stmt->bindParam(':COMPANY EMPLOYEES NUMBER', $userId_employee, PDO::PARAM_INT);
-        $stmt->bindParam(':PASSWORD', $password_employee, PDO::PARAM_STR);
-      }else{
-      }
-      //SQL実行
-      $stmt->execute();
+    $sql = 'SELECT * FROM COMPANY EMPLOYEES';
 
-      if($stmt->rowCount() > 0){
-        header('Location home_employee.php');
-        exit();
-      }
+    $where = "where COMPANY EMPLOYEES NUMBER = :COMPANY EMPLOYEES NUMBER";
+    $AND = "AND PASSWORD = :PASSWORD";
 
-      // echo '<pre>';
-      // var_dump($result);
-      // echo '</pre>';
-  
-    }catch(PDOException $e){
-      echo'error:'.$e->getMessage();
-    }finally{
-      
-      //この上は省略
-      //接続切断処理
-      $stmt = null;
-      $db = null;
+    $stmt = $db->prepare($sql . $where . $AND);
+    if ($userId_employee && $password_employee) {
+      //値のバインド
+      $stmt->bindParam(':COMPANY EMPLOYEES NUMBER', $userId_employee, PDO::PARAM_INT);
+      $stmt->bindParam(':PASSWORD', $password_employee, PDO::PARAM_STR);
+    } else {
     }
-}else if(isset($_POST["submitBtn2"])){
-  try{
+    //SQL実行
+    $stmt->execute();
+
+    if ($stmt->rowCount() > 0) {
+      header('Location home_employee.php');
+      exit();
+    }
+
+    // echo '<pre>';
+    // var_dump($result);
+    // echo '</pre>';
+
+  } catch (PDOException $e) {
+    echo 'error:' . $e->getMessage();
+  } finally {
+
+    //この上は省略
+    //接続切断処理
+    $stmt = null;
+    $db = null;
+  }
+} else if (isset($_POST["submitBtn2"])) {
+  try {
 
     $db = new PDO($dsn, DB_USER, DB_PASS);
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
@@ -78,25 +79,24 @@ if(isset($_POST["submitBtn1"])){
     $where = "where CUSTOMER NUMBER = :CUSTOMER NUMBER";
     $AND = "AND PASSWORD = :PASSWORD";
 
-    $stmt = $db->prepare($sql.$where.$AND);
-    if($userId_customer && $password_customer){
+    $stmt = $db->prepare($sql . $where . $AND);
+    if ($userId_customer && $password_customer) {
       //値のバインド
       $stmt->bindParam(':CUSTOMER NUMBER', $userId_customer, PDO::PARAM_INT);
       $stmt->bindParam(':PASSWORD', $password_customer, PDO::PARAM_STR);
-    }else{
+    } else {
     }
     //SQL実行
     $stmt->execute();
 
-    if($stmt->rowCount() > 0){
+    if ($stmt->rowCount() > 0) {
       header('Location customerform.php');
       exit;
     }
+  } catch (PDOException $e) {
+    echo 'error:' . $e->getMessage();
+  } finally {
 
-  }catch(PDOException $e){
-    echo'error:'.$e->getMessage();
-  }finally{
-    
     //この上は省略
     //接続切断処理
     $stmt = null;
@@ -106,44 +106,49 @@ if(isset($_POST["submitBtn1"])){
 ?>
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="./fairiesproject_login.css">
-    <title>従業員画面</title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="./fairiesproject_login.css">
+  <title>従業員画面</title>
 </head>
+
 <body>
   <form method="POST">
-        <header>
-            <h1>
-                <img src="images/fairieshome.png" alt="ロゴ" width="280">
-            </h1>
-        </header> 
-        <main>
-            <h2>災害連絡掲示板</h2>
-            <nav>
-                <div class="nav-menu">
-                  <button id="loginBtn1">従業員の方</button>
-                  <button id="loginBtn2">お客様はこちら</button>
-            </nav>
-        </main>
+    <header>
+      <h1>
+        <img src="images/fairieshome.png" alt="ロゴ" width="280">
+      </h1>
+    </header>
+    <main>
+      <div class="title">
+        <img src="images/title_img.png" alt="title">
+      </div>
+      <h2>災害連絡掲示板</h2>
+      <nav>
+        <div class="nav-menu">
+          <button id="loginBtn1">従業員の方</button>
+          <button id="loginBtn2">お客様はこちら</button>
+      </nav>
+    </main>
     <dialog id="loginDialog_employee">
-        <div id="loginWrapper">
-          <header id="loginHeader">
-            <h2>ログイン画面</h2>
-            <button type="button" id="closeBtn1">x</button>
-          </header>
-          <div>
-            <label for="userId_employee">社員番号:</label>
-            <input type="text" placeholder="社員番号入力してください" name="userId_employee" id="userId_employee">
-          </div>
-          <div>
-            <label for="password_employee">パスワード:</label>
-            <input type="password" placeholder="パスワード入力してください" name="password_employee" id="password_employee">
-          </div>
-          <div><a href="./fairiesproject_register.php">新規登録はこちら</a></div>       <!--新規作成画面に移動-->
-          <button type="submit" id="submitBtn1" name="submitBtn1">ログイン</button>
+      <div id="loginWrapper">
+        <header id="loginHeader">
+          <h2>ログイン画面</h2>
+          <button type="button" id="closeBtn1">x</button>
+        </header>
+        <div>
+          <label for="userId_employee">社員番号:</label>
+          <input type="text" placeholder="社員番号入力してください" name="userId_employee" id="userId_employee">
         </div>
-    </dialog>           <!--従業員用ダイアログ-->
+        <div>
+          <label for="password_employee">パスワード:</label>
+          <input type="password" placeholder="パスワード入力してください" name="password_employee" id="password_employee">
+        </div>
+        <div><a href="./fairiesproject_register.php">新規登録はこちら</a></div> <!--新規作成画面に移動-->
+        <button type="submit" id="submitBtn1" name="submitBtn1">ログイン</button>
+      </div>
+    </dialog> <!--従業員用ダイアログ-->
     <dialog id="loginDialog_customer">
       <div id="loginWrapper">
         <header id="loginHeader">
@@ -158,11 +163,12 @@ if(isset($_POST["submitBtn1"])){
           <label for="password_customer">パスワード:</label>
           <input type="password" placeholder="パスワード入力してください" name="password_customer" id="password_customer">
         </div>
-        <div><a href="./fairiesproject_register.php">新規登録はこちら</a></div>       <!--新規作成画面に移動-->
+        <div><a href="./fairiesproject_register.php">新規登録はこちら</a></div> <!--新規作成画面に移動-->
         <button type="submit" id="submitBtn2" name="submitBtn2">ログイン</button>
       </div>
-    </dialog>            <!--お客様用ダイアログ-->
+    </dialog> <!--お客様用ダイアログ-->
     <script src="fairiesproject_login.js"></script>
   </form>
 </body>
+
 </html>
