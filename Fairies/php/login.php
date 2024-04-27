b97088a692550f4bf3f891ccbe9f531495b231cb
haruna <haru_kawagishi@icloud.com>
2024-04-28 00:16:37 +0900

    forの中身の変更

--- Fairies/php/login.php
+++ Fairies/php/login.php
@@ -135,28 +135,31 @@ if (isset($_POST["submitBtn1"])) {
 
         <div><a href="./user_register.php">新規登録はこちら</a></div> <!--新規作成画面に移動-->
 
-        <!-- ログインで表示する内容 -->
+        <!-- 従業員のログイン表示-->
         <div class="employee">
           <div class="employee_number">
             <label for="employee_number">社員番号を入力してください</label>
-            <input type="text" placeholder="12345" name="userId_employee" id="userId_employee">
+            <input type="text" placeholder="12345"  id="userId_employee">
           </div>
+
           <div class="employee_pass">
-            <label for="employee_pass">パスワードを入力してください:</label>
-            <input type="password" placeholder="eccComp2024" name="password_employee" id="password_employee">
+            <label for="employee_pass">パスワードを入力してください</label>
+            <input type="password" placeholder="eccComp2024"id="password_employee">
           </div>
 
           <button type="submit" id="submitBtn1" name="submitBtn1">ログイン</button>
         </div>
+
+        <!-- お客様のログイン表示 -->
         <div class="customer">
 
           <div class="customer_number">
-            <label for="customer_number">お客様番号を入力してください:</label>
-            <input type="text" placeholder="12345" name="userId_customer" id="userId_customer">
+            <label for="customer_number">お客様番号を入力してください</label>
+            <input type="text" placeholder="12345" id="userId_customer">
           </div>
           <div class="customer_pass">
             <label for="customer_pass">パスワード入力してください</label>
-            <input type="password" placeholder="eccComp2024" name="password_customer" id="password_customer">
+            <input type="password" placeholder="eccComp2024" id="password_customer">
           </div>
           <button type="submit" id="submitBtn2" name="submitBtn2">ログイン</button>
         </div>
